fe2a1d54c1a019d21ed6026f064030a9390f6229
prince37 <prince@binaryveda.com>
2022-10-20 13:46:06 +0530

    [ Testing ]

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by Prince Maurya
         </a>
       </header>
     </div>
